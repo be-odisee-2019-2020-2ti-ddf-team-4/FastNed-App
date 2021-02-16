74648c12a59767d5413cec0dea007f00e5d08b81
pacifiquebayisenge <32477950+pacifiquebayisenge@users.noreply.github.com>
2021-02-16 09:55:56 +0100

    clean

--- fastned-vue-front/src/views/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>About Fastned Client</h1>
-  </div>
-</template>
--- fastned-vue-front/src/views/FastNed.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div id="fastned">
-    <h1>Welkom!</h1>
-
-    <MainForm />
-
-  </div>
-</template>
